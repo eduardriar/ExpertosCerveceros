dcf6e4f53d0c09322d2316ead13bd47063344756
eduardriar <eduardriar@unisabana.edu.co>
2020-05-12 11:55:13 -0500

    Cambios

--- App.js
+++ App.js
@@ -5,40 +5,184 @@ import { View, Text, Button } from 'react-native';
 import SplashScreen from 'react-native-splash-screen'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Login from './Components/Login/Login'
+import Search from './Components/Search'
+import Premium from './Components/Premium'
+import Profile from './Components/Profile'
+import Home from './Components/Home/Home'
+import AsyncStorage from '@react-native-community/async-storage';
+import { Header } from 'react-native/Libraries/NewAppScreen';
 
-function HomeScreen({ navigation }) {
+const Tab = createMaterialBottomTabNavigator();
+
+function HomeTab({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Search')}
-      />
-    </View>
+    <Tab.Navigator
+      initialRouteName="Start"
+      activeColor="#EC40F0"
+      inactiveColor="#EC40F0"
+      barStyle={{ backgroundColor: '#FFFFFF' }}>
+      <Tab.Screen name="Start"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={'#EC40F0'} size={26} />
+          ),
+        }} />
+      <Tab.Screen name="Search" component={SearchScreen}
+        options={{
+          tabBarLabel: 'Search',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="magnify" color={'#EC40F0'} size={26} />
+          ),
+        }} />
+      <Tab.Screen name="Premium" component={PremiumScreen}
+        options={{
+          tabBarLabel: 'Premium',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="star" color={'#EC40F0'} size={26} />
+          ),
+        }} />
+      <Tab.Screen name="Profile" component={ProfileScreen}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="beer" color={'#EC40F0'} size={26} />
+          ),
+        }} />
+    </Tab.Navigator>
   );
 }
 
+function HomeScreen() {
+  return (<Home />)
+}
+
+function SearchScreen() {
+  return (<Search />)
+}
+
+function PremiumScreen() {
+  return (<Premium />)
+}
+
+function ProfileScreen() {
+  return (<Profile />)
+}
+
 function LoginScreen() {
-  return (<Login/>)
+  return (<Login />)
 }
 
 const Stack = createStackNavigator();
 
 function App() {
-  useEffect(()=>{
+  const [state, dispatch] = React.useReducer(
+    (prevState, action) => {
+      switch (action.type) {
+        case 'RESTORE_TOKEN':
+          return {
+            ...prevState,
+            userName: action.token,
+            isLoading: false,
+          };
+        case 'SIGN_IN':
+          return {
+            ...prevState,
+            isSignout: false,
+            userName: action.token,
+          };
+        case 'SIGN_OUT':
+          return {
+            ...prevState,
+            isSignout: true,
+            userName: null,
+          };
+      }
+    },
+    {
+      isLoading: true,
+      isSignout: false,
+      userToken: null,
+    }
+  );
+
+  React.useEffect(() => {
+    // Fetch the token from storage then navigate to our appropriate place
+    const bootstrapAsync = async () => {
+      let userInfo;
+      let userName;
+
+      try {
+        userInfo = await AsyncStorage.getItem('userInfo');
+        userName = JSON.parse(userInfo)
+      } catch (e) {
+        // Restoring token failed
+      }
+
+      // After restoring token, we may need to validate it in production apps
+
+      // This will switch to the App screen or Auth screen and this loading
+      // screen will be unmounted and thrown away.
+      dispatch({ type: 'RESTORE_TOKEN', token: userName });
+    };
+
+    bootstrapAsync();
+  }, []);
+
+  const authContext = React.useMemo(
+    () => ({
+      signIn: async data => {
+        // In a production app, we need to send some data (usually username, password) to server and get a token
+        // We will also need to handle errors if sign in failed
+        // After getting token, we need to persist the token using `AsyncStorage`
+        // In the example, we'll use a dummy token
+
+        let userInfo = await AsyncStorage.getItem('userInfo');
+        let userName = JSON.parse(userInfo)
+
+        dispatch({ type: 'SIGN_IN', token: userName });
+      },
+      signOut: () => dispatch({ type: 'SIGN_OUT' }),
+      signUp: async data => {
+        // In a production app, we need to send user data to server and get a token
+        // We will also need to handle errors if sign up failed
+        // After getting token, we need to persist the token using `AsyncStorage`
+        // In the example, we'll use a dummy token
+
+        let userInfo = await AsyncStorage.getItem('userInfo');
+        let userName = JSON.parse(userInfo)
+
+        dispatch({ type: 'SIGN_IN', token: userName });
+      },
+    }),
+    []
+  );
+
+  useEffect(() => {
     SplashScreen.hide();
   }, []);
 
+  const AuthContext = React.createContext();
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login">
-          {props => <LoginScreen {...props} />}
-        </Stack.Screen>
-        <Stack.Screen name="Home" component={HomeScreen}  />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthContext.Provider value={authContext}>
+      {console.log(state.userName)}
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{
+          headerShown: false
+        }}>
+          {state.userName == null ? (
+            <Stack.Screen name="Login" component={LoginScreen}/>
+          ) : (
+              <Stack.Screen name="Home" component={HomeTab} />
+            )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthContext.Provider>
   );
 }
 
--- Components/Home/Home.js
+++ Components/Home/Home.js
@@ -1,88 +1,134 @@
 /* eslint-disable prettier/prettier */
 
-import React, { useEffect } from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useEffect, Component } from 'react';
+import { View, Text, Button, FlatList, SafeAreaView, Image, Dimensions } from 'react-native';
 import SplashScreen from 'react-native-splash-screen'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import Search from './Components/Search'
-import Profile from './Components/Profile'
-import Premium from './Components/Premium'
-import Login from './Components/Login/Login';
+import Search from '../Search';
+import Premium from '../Premium';
+import Profile from '../Profile';
+import styles from './HomeStyle';
+import Item from './Item';
+import { ScrollView } from 'react-native-gesture-handler';
 
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Search')}
-      />
-    </View>
-  );
-}
+const WIDTH = Dimensions.get('window').width;
+const HEIGHT = Dimensions.get('window').height;
+const sizeH = HEIGHT / 100;
+const sizeW = WIDTH / 100;
 
-function SearchScreen() {
-  return (<Search />)
-}
+const beer = [
+  { img: 'http://www.gecsas.com.co/ImagesECommerce/Cerveza/Cerveza1.png', name: 'Estrella Damm', id: '1' },
+  { img: 'http://www.gecsas.com.co/ImagesECommerce/Cerveza/Cerveza2.png', name: 'Marantá', id: '2' },
+  { img: 'http://www.gecsas.com.co/ImagesECommerce/Cerveza/Cerveza3.png', name: 'Becks', id: '3' },
+  { img: 'http://www.gecsas.com.co/ImagesECommerce/Cerveza/Cerveza4.png', name: 'Bitburger', id: '4' },
+  { img: 'http://www.gecsas.com.co/ImagesECommerce/Cerveza/Cerveza5.png', name: 'Maredsous', id: '5' },
+  { img: 'http://www.gecsas.com.co/ImagesECommerce/Cerveza/Cerveza6.png', name: 'Erdinger', id: '6' },
+  { img: 'http://www.gecsas.com.co/ImagesECommerce/Cerveza/Cerveza7.png', name: '3 Cordilleras', id: '7' }]
 
-function PremiumScreen() {
-  return (<Premium />)
-}
+const receipts = [
+  { img: 'http://www.gecsas.com.co/ImagesECommerce/Maridaje/Receta1.png', name: 'Estrella Damm', id: '8' },
+  { img: 'http://www.gecsas.com.co/ImagesECommerce/Maridaje/Receta2.png', name: 'Estrella Damm', id: '9' },
+  { img: 'http://www.gecsas.com.co/ImagesECommerce/Maridaje/Receta3.png', name: 'Estrella Damm', id: '10' },
+  { img: 'http://www.gecsas.com.co/ImagesECommerce/Maridaje/Receta4.png', name: 'Estrella Damm', id: '11' },
+]
 
-function ProfileScreen() {
-  return (<Profile />)
-}
+const maridaje = [
+  { img: 'http://www.gecsas.com.co/ImagesECommerce/Maridaje/Maridaje1.png', name: 'Estrella Damm', id: '11' },
+  { img: 'http://www.gecsas.com.co/ImagesECommerce/Maridaje/Maridaje2.png', name: 'Estrella Damm', id: '12' },
+  { img: 'http://www.gecsas.com.co/ImagesECommerce/Maridaje/Maridaje3.png', name: 'Estrella Damm', id: '13' },
+  { img: 'http://www.gecsas.com.co/ImagesECommerce/Maridaje/Maridaje4.png', name: 'Estrella Damm', id: '14' },
+  { img: 'http://www.gecsas.com.co/ImagesECommerce/Maridaje/Maridaje5.png', name: 'Estrella Damm', id: '15' },
+]
 
-const Tab = createMaterialBottomTabNavigator();
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      enableScrollViewScroll: true,
+    };
+  }
 
-function Home() {
+  onEnableScroll = (value) => {
+    this.setState({
+      enableScrollViewScroll: value,
+    });
+  };
 
-  useEffect(() => {
-    SplashScreen.hide();
-  }, []);
-
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Home"
-        activeColor="#EC40F0"
-        inactiveColor="#EC40F0"
-        barStyle={{ backgroundColor: '#FFFFFF' }}>
-        <Tab.Screen name="Home"
-          component={HomeScreen}
-          options={{
-            tabBarLabel: 'Home',
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="home" color={'#EC40F0'} size={26} />
-            ),
-          }} />
-        <Tab.Screen name="Search" component={SearchScreen}
-          options={{
-            tabBarLabel: 'Search',
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="magnify" color={'#EC40F0'} size={26} />
-            ),
-          }} />
-        <Tab.Screen name="Premium" component={PremiumScreen}
-          options={{
-            tabBarLabel: 'Premium',
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="star" color={'#EC40F0'} size={26} />
-            ),
-          }} />
-        <Tab.Screen name="Profile" component={ProfileScreen}
-          options={{
-            tabBarLabel: 'Profile',
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="beer" color={'#EC40F0'} size={26} />
-            ),
-          }} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+  render() {
+    return (
+      <View style={styles.sectionContainer}>
+        <View style={styles.header}>
+          <View style={styles.headerTop}>
+            <View style={styles.expertosCerveceros}>
+              <Text style={styles.expertos}> Expertos</Text>
+              <Text style={styles.cerveceros}> CERVECEROS</Text>
+            </View>
+          </View>
+          <View style={styles.headerBottom}>
+            <View style={styles.square}>
+              <Text style={styles.squareOne}>Square1</Text>
+              <Text style={styles.squareTwo}>Square2</Text>
+            </View>
+          </View>
+        </View>
+        <View style={styles.containerScroll}>
+          <ScrollView nestedScrollEnabled={true} style={styles.scrollView}>
+            <View style={styles.containerCards}>
+              <Text style={styles.label}>LAS CERVEZAS DE NUESTRAS RECETAS</Text>
+              <FlatList
+                nestedScrollEnabled={true}
+                style={{ marginTop: HEIGHT * 0.03 }}
+                horizontal={true}
+                data={beer}
+                renderItem={({ item }) => (
+                  <Item
+                    id={item.id}
+                    img={item.img}
+                  />
+                )}
+                keyExtractor={item => item.id}
+              />
+            </View>
+            <View style={styles.containerCards}>
+              <Text style={styles.label}>MARIDAJE</Text>
+              <FlatList
+                nestedScrollEnabled={true}
+                style={{ marginTop: HEIGHT * 0.03 }}
+                horizontal={true}
+                data={maridaje}
+                renderItem={({ item }) => (
+                  <Item
+                    id={item.id}
+                    img={item.img}
+                  />
+                )}
+                keyExtractor={item => item.id}
+              />
+            </View>
+            <View style={styles.containerCards}>
+              <Text style={styles.label}>RECETAS POPULARES</Text>
+              <FlatList
+                style={{ marginTop: HEIGHT * 0.03 }}
+                horizontal={false}
+                numColumns={'2'}
+                data={receipts}
+                renderItem={({ item }) => (
+                  <Item
+                    id={item.id}
+                    img={item.img}
+                  />
+                )}
+                keyExtractor={item => item.id}
+              />
+            </View>
+          </ScrollView>
+        </View>
+      </View>
+    );
+  }
 }
 
 export default Home;
